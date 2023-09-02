c3e87bb0c25970de56dc3252c2d50ceef678104e
TejaAndagundala <andagundalateja@gmail.com>
2023-09-02 10:15:48 +0530

    C4 Searchbar

---
+++ src/components/SearchBar/Search.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import styles from "./Search.module.css";
+import { ReactComponent as SearchIcon } from "../../assests/search-icon.svg";
+
+function Search({ placeholder }) {
+  return (
+    <div>
+      <form className={styles.wrapper}>
+        <input className={styles.search} placeholder={placeholder} />
+        <button className={styles.searchButton} type="submit">
+          <SearchIcon />
+        </button>
+      </form>
+    </div>
+  );
+}
+
+export default Search;
