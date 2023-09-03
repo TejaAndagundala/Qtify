134bbcce956ac60cba261c6eafb364d24870c534
TejaAndagundala <andagundalateja@gmail.com>
2023-09-03 11:36:01 +0530

    qtify-herosection-takehome

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-import Navbar from "./components/Navbar/navbar";
+import Hero from "./components/Hero/Hero.jsx";
+import Navbar from "./components/Navbar/Navbar.jsx";
 
 function App() {
   return (
     <>
       <Navbar />
+      <Hero />
     </>
   );
 }
